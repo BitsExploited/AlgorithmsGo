5d8bb61acf9ceb2f0920b97f6d9bad5d96b5b0dd
BitsExploited <tspamiitesh@gmail.com>
2025-05-26 16:02:00 +0530

    Adding luhn number validation algorithm

--- checksum/luhn_validation.go
+++ checksum/luhn_validation.go
@@ -1,3 +1,15 @@
+/*
+Time Complexity: O(n)
+
+(From right) → 7, 6, 4, 6, 3, 4, 3, 0, 8, 8, 4, 1, 9, 3, 5, 4
+Double these:    ↑   ↑   ↑   ↑   ↑   ↑   ↑   ↑
+Becomes:         7, 12, 4, 12, 3, 8, 3, 0, 8, 16, 4, 2, 9, 6, 5, 8
+Then subtract 9 from values > 9:
+                 7, 3, 4, 3, 3, 8, 3, 0, 8, 7, 4, 2, 9, 6, 5, 8
+Sum: **70**
+→ `70 % 10 == 0` → Valid
+*/
+
 package main
 
 import (
